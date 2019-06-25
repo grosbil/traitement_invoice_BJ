d1d5b6b985fdb557c5de8754f95934e333043957
grosbil <ochassing@gmail.com>
2019-06-25 17:17:55 +0200

    duplication des fichier invoice

--- TraitementInvoiceCSIWeb/Program.cs
+++ TraitementInvoiceCSIWeb/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+
 
 namespace TraitementInvoiceCSIWeb
 {
@@ -13,33 +11,40 @@ namespace TraitementInvoiceCSIWeb
     {
         static void Main(string[] args)
         {
-            string PathFileIn = "invoicetodownload.txt";
-
-
+            
+           
+            var appSettings = ConfigurationManager.AppSettings;
+            string Cronkey = appSettings["key"];
+            string PathFileIn = appSettings["file"];
+            string pathInvoice = appSettings["pathInvoice"];
             string[] fichier = File.ReadAllText(PathFileIn).Split(';');
-
             for (int i = 0; i < fichier.Length - 1; i += 2)
             {
 
                 ArrayList j = new ArrayList();
                 j.Add(fichier[i]);
                 j.Add(fichier[i + 1]);
-                Console.WriteLine(fichier.Length);
-                Console.WriteLine("le premier élement de la liste :" + j[0]);
-                Console.WriteLine("le deuxième élement de la liste :" + j[1]);
-                // Console.ReadKey();
+                //Console.WriteLine(fichier.Length);
+                //Console.WriteLine("le premier élement de la liste :" + j[0]);
+                //Console.WriteLine("le deuxième élement de la liste :" + j[1]);
+                //// Console.ReadKey();
 
-                string remoteUri = "https://www.compressport.com/inter/module/sage123/invoice?cronkey="+args[0]+ "&reference = YYYY" + j[1];
-                //string fileName = j[0]+".pdf", myStringWebResource = null; 
-                string fileName = j[0] + ".pdf", myStringWebResource = null;
+                string remoteUri = "https://www.compressport.com/inter/module/sage123/invoice?cronkey="+Cronkey+ "&reference=" + j[1];
+                string fileName_a = j[0] + "a.pdf";
+                string fileName_b = j[0] + "b.pdf";
+                string fileName_c = j[0] + "c.pdf";
+                string fileName_d = j[0] + "d.pdf", myStringWebResource = null;
                 // Create a new WebClient instance.
                 WebClient myWebClient = new WebClient();
                 // Concatenate the domain with the Web resource filename.
                 myStringWebResource = remoteUri;
-                Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
+                //Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
                 // Download the Web resource and save it into the current filesystem folder.
-                myWebClient.DownloadFile(myStringWebResource, fileName);
-                Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
+                myWebClient.DownloadFile(myStringWebResource, pathInvoice+fileName_a);
+                myWebClient.DownloadFile(myStringWebResource, pathInvoice + fileName_b);
+                myWebClient.DownloadFile(myStringWebResource, pathInvoice + fileName_c);
+                myWebClient.DownloadFile(myStringWebResource, pathInvoice + fileName_d);
+                //Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
                 //Console.WriteLine("\nDownloaded file saved in the following file system folder:\n\t" + Application.StartupPath);
                 // Console.ReadKey();
             }
